86eab97e81af0fdd04a3255b1fb1cdf428244ded
Vinicius Martins <viniciusmvg@hotmail.com>
2026-08-18 18:30:44 -0300

    Carregando o vídeo na primeira página

---
+++ script.js
@@ -0,0 +1,9 @@
+
+window.addEventListener("load", ()=>{
+    const video = document.querySelector(".videohero");
+    video.src = "img/video-hero.mp4";
+    video.autoplay = true;
+    video.loop = true;
+    video.muted = true;
+})
+
