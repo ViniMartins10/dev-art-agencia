76127e15187c31b890ceb4cc71ff2ffbcf5639c8
Vinicius Martins <viniciusmvg@hotmail.com>
2026-08-19 18:51:01 -0300

    Criando as divs da transição

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 
 window.addEventListener("load", ()=>{
     const video = document.querySelector(".videohero");
+    const videofooter = document.querySelector(".videofooter");
+    console.log(videofooter)
+
+
     video.src = "img/video-hero.mp4";
     video.autoplay = true;
     video.loop = true;
     video.muted = true;
+
+    videofooter.src = "img/video-footer.mp4";
+    videofooter.autoplay = true;
+    videofooter.loop = true;
+    videofooter.muted = true;
 })
+
+
 
