b6811a3fd7b21a7b4a9f4673c54115716b5750f2
Vinicius Martins <viniciusmvg@hotmail.com>
2026-08-25 17:28:16 -0300

    Criando as animações dos textos surgindo

--- script.js
+++ script.js
@@ -24,17 +24,17 @@ window.addEventListener("load", ()=>{
     const linhadotempo = gsap.timeline({
         scrollTrigger: {
             trigger: ".transicao",
-            markers: true,
+            markers:false,
             scrub: 3,
             start: "0% 0%",
             end: "+=3000",
             pin: true,
         },
-});
+    });
     
     linhadotempo.to(".retangulos div", {
         y: 0,
-        stagger: .2,
+        stagger: 0.2,
         duration: 4
     });
 
@@ -51,6 +51,48 @@ window.addEventListener("load", ()=>{
         stagger: .08,
         duration: 1
     });
+
+    //ANIMAÇÕES TEXTOS SURGINDO
+   const linhaDoTempo2 = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".secao4",
+            markers: true,
+            scrub: 2, //começa a animação quando o elemento entra na tela e termina quando sai da tela
+            pin: true, // deixa a pagina travada em quanto a animação estiver acontecendo
+
+        }
+   }) 
+   
+    // linhaDoTempo2.to(".secao4 h2:nth-child(1)", {
+    //     opacity: 1,
+    // });
+    // linhaDoTempo2.to(".secao4 h2:nth-child(1)", {
+    //     opacity: 0,
+    // });
+
+    // linhaDoTempo2.to(".secao4 h2:nth-child(2)", {
+    //     opacity: 1,
+    // });
+    // linhaDoTempo2.to(".secao4 h2:nth-child(2)", {
+    //     opacity: 0,
+    // });
+
+    // linhaDoTempo2.to(".secao4 h2:nth-child(3)", {
+    //     opacity: 1,
+    // });
+    // linhaDoTempo2.to(".secao4 h2:nth-child(3)", {
+    //     opacity: 0,
+    // }); FORMA BRUTA QUE FUNCIONA
+
+    const textoSecao4 = document.querySelectorAll(".secao4 h2"); // vai pegar todos os elementos h2 dentro da secao4
+    textoSecao4.forEach((texto) => {
+        linhaDoTempo2.to(texto, {
+            opacity: 1,
+        })
+        linhaDoTempo2.to(texto, {
+            opacity: 0,
+        })
+    }); // vai percorrer todos os elementos h2
 });
 
 
